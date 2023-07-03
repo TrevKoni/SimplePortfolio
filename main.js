c1b7f3fb51f03a19508f1e4cef52e6bfaef0d5b4
TrevKoni <konitrev@gmail.com>
2023-07-03 20:36:55 +0200

    Small fixes

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-import './style.css'
-import './sidebar.css'
+import './style.css';
+import './sidebar.css';
+import './bodycolors.js';
+import './sidebar.js';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
