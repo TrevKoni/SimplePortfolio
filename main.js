75f0ea6e27b4b9cbee4435190d6276da7e2786ee
TrevKoni <konitrev@gmail.com>
2023-07-12 13:25:43 +0200

    Bug Fixes and Edits

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import './style.css';
 import './sidebar.css';
 import './bodycolors.js';
-import './sidebar.js';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
